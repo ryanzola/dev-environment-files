21a5a0c1cd8492ae2a122c74ef1959acccb29e09
ryanzola <ryanzola@me.com>
2022-12-28 22:20:39 -0500

    :tada: initial commit

---
+++ .config/nvim/lua/ryanzola/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  return
+end
+
+treesitter.setup({
+  highlight = {
+    enable = true
+  },
+  indent = { enable = true },
+  autotag = { enable = true },
+  ensure_installed = {
+    "json",
+    "javascript",
+    "typescript",
+    "tsx",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "svelte",
+    "graphql",
+    "bash",
+    "lua",
+    "vim",
+    "vue",
+    "dockerfile",
+    "gitignore",
+    "glsl",
+    "go",
+    "rust",
+  },
+  auto_install = true,
+})
